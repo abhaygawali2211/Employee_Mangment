37c3c130aa48cf0e4749c79dcbc2a34485b296f9
abhaygawali2211 <117912101+abhaygawali2211@users.noreply.github.com>
2026-05-11 12:34:10 +0530

    Update index.js

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -133,7 +133,7 @@ const leaveApplicationReminder = inngest.createFunction(
 const attendanceReminderCron = inngest.createFunction(
   {
     id: "attendance-reminder-cron",
-    triggers: [{ cron: " TZ=Asia/Kolkata 30 11 * * *" }],
+    triggers:[{ cron: "0 6 * * *" }],
   },
   async ({ step }) => {
     const today = await step.run("get-today-date", () => {
